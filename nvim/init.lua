cd01cfd4c3854ae31c4518fc14cdc8650487afe2
redus <6439332+redus@users.noreply.github.com>
2024-05-11 00:48:51 -0700

    color scheme settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,4 @@
 require('options')
+require('keymaps')
+require('plugins')
+require('colorscheme')
---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,25 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+-- vim.keymap.set('v', '<', '<gv', opts)
+-- vim .keymap.set('v', '>', '>gv', opts)
+
