8272562e0adf02b36d871052e6bd417ffa76ebeb
redus <6439332+redus@users.noreply.github.com>
2024-05-20 23:28:15 -0700

    transparent background

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,9 +1,16 @@
 -- define your colorscheme here
-local colorscheme = 'catppuccin'
+local colorscheme = "catppuccin"
 
-local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+local is_ok, _ = pcall(require, colorscheme)
 if not is_ok then
     vim.notify('colorscheme ' .. colorscheme .. ' not found!')
     return
 end
+
+require(colorscheme).setup({
+    transparent_background = true,
+})
+
+vim.cmd('colorscheme ' .. colorscheme)
 
